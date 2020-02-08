c2e9c8f67e8db82f74c74ecf8438a7a04dd8ed74
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-08 14:04:19 +0000

    Creates function that shortens notes to 20 characters

--- spec/note-list-view-test.js
+++ spec/note-list-view-test.js
@@ -2,7 +2,7 @@
   var list = new List()
   list.newNote("Favourite food: pesto")
   list.newNote("Favourite drink: seltzer")
-  var view = new view()
+  var view = new View()
   view.inputList(list)
   view.modifyElements()
   assert.isTrue(view.createString() === "<ul><li><div>Favourite food: pest</div></li><li><div>Favourite drink: sel</div></li></ul>")
